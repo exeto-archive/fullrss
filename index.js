bb16249e965645efcf6a2d70f0ac15399a8edd92
Timofey Dergachev <t@exeto.me>
2015-10-27 17:04:37 +0300

    Updated .eslintrc and refactoring code

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const co           = require('co');
 const feedParse    = require('./lib/feed-parse');
 const generateFeed = require('./lib/generate-feed');
 
-module.exports = function(args) {
-  return co(function*() {
-    let result = yield feedParse(args.feed, args.max);
+module.exports = function (args) {
+  return co(function* () {
+    const result = yield feedParse(args.feed, args.max);
     return yield generateFeed(result, args.elements, args.dropElements,
-                              args.token, args.pureHtml);
+      args.token, args.pureHtml);
   });
 };
