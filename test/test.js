f80b58d4df97f0b73c2d0939d10b22f83f00c95a
Timofey Dergachev <t@exeto.me>
2016-12-28 19:09:39 +0300

    Migration from ava to jest

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import test from 'ava';
 import pify from 'pify';
 import nock from 'nock';
 import fullrss from '../src';
@@ -14,7 +13,7 @@ const readFile = async (filePath) => {
 
 let s;
 
-test.before('setup', async () => {
+beforeAll(async () => {
   nock('https://mercury.postlight.com')
     .filteringPath(/\?url=[^&]*/g, '')
     .get('/parser')
@@ -25,7 +24,7 @@ test.before('setup', async () => {
   await s.listen(s.port);
 });
 
-test('equal feed', async (t) => {
+test('equal feed', async () => {
   let feed = await fullrss({
     uri: `${s.url}/beforeFeed.xml`,
     target: '.test2, .test',
@@ -34,10 +33,10 @@ test('equal feed', async (t) => {
 
   feed = feed.replace(/<lastBuildDate>.*/, '');
 
-  t.is(feed, await readFile('fixtures/afterFeed.xml', 'utf-8'));
+  expect(await readFile('fixtures/afterFeed.xml', 'utf-8')).toBe(feed);
 });
 
-test('missing elements', async (t) => {
+test('missing elements', async () => {
   let feed = await fullrss({
     uri: `${s.url}/beforeFeed.xml`,
     target: '.missing',
@@ -45,48 +44,42 @@ test('missing elements', async (t) => {
 
   feed = feed.replace(/<lastBuildDate>.*/, '');
 
-  t.is(feed, await readFile('fixtures/afterFeedMissing.xml', 'utf-8'));
+  expect(await readFile('fixtures/afterFeedMissing.xml', 'utf-8')).toBe(feed);
 });
 
-test('missing token', async (t) => {
+test('missing token', async () => {
   try {
     await fullrss({
       uri: `${s.url}/beforeFeed.xml`,
     });
-
-    t.fail('Exception was not thrown');
   } catch (err) {
-    t.is(err.message, 'Missing Mercury token');
+    expect(err.message).toBe('Missing Mercury token');
   }
 });
 
-test('error 404', async (t) => {
+test('error 404', async () => {
   try {
     await fullrss({
       uri: `${s.url}/404`,
       target: 'p',
     });
-
-    t.fail('Exception was not thrown');
   } catch (err) {
-    t.is(err.message, 'Response code 404 (Not Found)');
+    expect(err.message).toBe('Response code 404 (Not Found)');
   }
 });
 
-test('not a feed', async (t) => {
+test('not a feed', async () => {
   try {
     await fullrss({
       uri: `${s.url}/post/1.html`,
       target: 'p',
     });
-
-    t.fail('Exception was not thrown');
   } catch (err) {
-    t.is(err.message, 'Not a feed');
+    expect(err.message).toBe('Not a feed');
   }
 });
 
-test('max items', async (t) => {
+test('max items', async () => {
   let feed = await fullrss({
     uri: `${s.url}/beforeFeed.xml`,
     target: '.test2, .test',
@@ -96,10 +89,10 @@ test('max items', async (t) => {
 
   feed = feed.replace(/<lastBuildDate>.*/, '');
 
-  t.is(feed, await readFile('fixtures/afterFeedMax.xml', 'utf-8'));
+  expect(await readFile('fixtures/afterFeedMax.xml', 'utf-8')).toBe(feed);
 });
 
-test('pure html', async (t) => {
+test('pure html', async () => {
   let feed = await fullrss({
     uri: `${s.url}/beforeFeed.xml`,
     target: '.test2, .test',
@@ -109,10 +102,10 @@ test('pure html', async (t) => {
 
   feed = feed.replace(/<lastBuildDate>.*/, '');
 
-  t.is(feed, await readFile('fixtures/afterFeedPure.xml', 'utf-8'));
+  expect(await readFile('fixtures/afterFeedPure.xml', 'utf-8')).toBe(feed);
 });
 
-test('encoding windws-1251', async (t) => {
+test('encoding windws-1251', async () => {
   let feed = await fullrss({
     uri: `${s.url}/beforeFeedWindows1251.xml`,
     target: 'p',
@@ -120,10 +113,10 @@ test('encoding windws-1251', async (t) => {
 
   feed = feed.replace(/<lastBuildDate>.*/, '');
 
-  t.is(feed, await readFile('fixtures/afterFeedWindows1251.xml', 'utf-8'));
+  expect(await readFile('fixtures/afterFeedWindows1251.xml', 'utf-8')).toBe(feed);
 });
 
-test('mercury', async (t) => {
+test('mercury', async () => {
   let feed = await fullrss({
     uri: `${s.url}/beforeFeed.xml`,
     mercuryToken: 'token',
@@ -131,10 +124,10 @@ test('mercury', async (t) => {
 
   feed = feed.replace(/<lastBuildDate>.*/, '');
 
-  t.is(feed, await readFile('fixtures/afterFeedMercury.xml', 'utf-8'));
+  expect(await readFile('fixtures/afterFeedMercury.xml', 'utf-8')).toBe(feed);
 });
 
-test('use a mercury absent elements', async (t) => {
+test('use a mercury absent elements', async () => {
   let feed = await fullrss({
     uri: `${s.url}/beforeFeed.xml`,
     target: '.missing',
@@ -143,9 +136,9 @@ test('use a mercury absent elements', async (t) => {
 
   feed = feed.replace(/<lastBuildDate>.*/, '');
 
-  t.is(feed, await readFile('fixtures/afterFeedMercury.xml', 'utf-8'));
+  expect(await readFile('fixtures/afterFeedMercury.xml', 'utf-8')).toBe(feed);
 });
 
-test.after('cleanup', async () => {
+afterAll(async () => {
   await s.close();
 });
